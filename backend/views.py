af168f041c53a4ff587a101f98095c1ade812215
Yosef Shemesh <sunshineyos@gmail.com>
2013-11-14 12:13:58 -0500

    Revised the method to fetch collecitons

--- backend/views.py
+++ backend/views.py
@@ -24,7 +24,8 @@ def list(request):
     req = urllib2.Request(url)
     base64string = base64.encodestring('%s:%s' % (auth['user_id'], auth['session'])).replace('\n', '')
     req.add_header("Authorization", "Basic %s" % base64string)
-    req.data = {"query":"curated:true type:bundle ","order":"title","reverse":false,"language":"en","limit":50,"offset":0}
+    
+    req.data = {"query":"curated:true type:bundle ","order":"title","reverse":False,"language":"en","limit":50,"offset":0}
     req.get_method = lambda: "POST"
 
     response = urllib2.urlopen(req)
