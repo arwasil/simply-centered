dbe56ff790e07684e3767fbc7892ad29e99359b2
Yosef Shemesh <sunshineyos@gmail.com>
2013-11-14 12:43:01 -0500

    Revised the method to fetch collecitons

--- backend/views.py
+++ backend/views.py
@@ -19,18 +19,19 @@ def authorization():
 def list(request):
     auth = authorization()
 
-    #url = 'https://simplycentered.trap.it/api/v3/sc/trapdumps/bundles/'
-    url = 'https://simplycentered.trap.it/api/v3/sc/search/?pretty=true'
+    url = 'https://simplycentered.trap.it/api/v3/sc/trapdumps/bundles/'
+    #url = 'https://simplycentered.trap.it/api/v3/sc/search/?pretty=true'
     req = urllib2.Request(url)
     base64string = base64.encodestring('%s:%s' % (auth['user_id'], auth['session'])).replace('\n', '')
     req.add_header("Authorization", "Basic %s" % base64string)
     
-    values = {"query":"curated:true type:bundle ","order":"title","reverse":False,"language":"en","limit":50,"offset":0}
+    #values = {"query":"curated:true type:bundle ","order":"title","reverse":False,"language":"en","limit":50,"offset":0}
     data = urllib.urlencode(values)
     #req.get_method = lambda: "POST"
+    #response = urllib2.urlopen(req, data)
 
     response = urllib2.urlopen(req, data)
-    
+
     data = simplejson.load(response)
 
     return render(request, 'backend/list.html', {"list": data})
