7446075fd228fc521a07b3233f010f89315e9548
Andrey Bulagin <abulagin@gmail.com>
2013-11-15 18:50:06 +0400

    'Add to Spling' button

---
+++ main/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+SPLING_CATEGORIES = {
+	"food": 265,
+	"cooking": 266,
+	"healthy diets": 267,
+	"recipes": 268,
+	"natural kitchen": 269,
+	"diets": 270,
+	"superfoods": 271,
+	"health": 272,
+	"beauty": 273,
+	"natural health": 274,
+	"inner beauty": 275,
+	"outer beauty": 276,
+	"wellness": 277,
+	"longevity": 278,
+	"fitness": 279,
+	"mindful fitness": 280,
+	"optimum body": 281,
+	"yoga": 282,
+	"alternative exercise": 283,
+}
