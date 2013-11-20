b0b99ee7431aac09b5dcc2caf5833ef4af73ae15
Andrey Bulagin <abulagin@gmail.com>
2013-11-20 13:29:51 +0400

    Admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from models import *
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent', 'spling_code', 'image')
+    ordering = ('parent', 'id')
+
+admin.site.register(Category, CategoryAdmin)
