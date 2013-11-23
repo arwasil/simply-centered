9404765c9e14a9b064f060e1bce0788d75de77b0
Andrey Bulagin <abulagin@gmail.com>
2013-11-23 19:43:15 +0400

    Use the spling recommendation engine for content

--- main/views.py
+++ main/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 
+import requests, simplejson as json
+
 from models import *
+
 
 def index(request):
   return render(request, 'main/index.html')
 
 def board(request, *slugs):
   category = get_object_or_404(Category, slug=slugs[-1])
-  return render(request, 'main/board.html', {'category': category})
+
+  url = 'http://spling.com/api/2/recommendation/?limit=6&format=json'
+  response = requests.get(url)
+  spling_data = response.json()
+  print json.dumps(spling_data['items'][0], indent=4*' ')
+
+  return render(request, 'main/board.html', {'category': category, 'data': spling_data['items']})
 
 def spling(request):
 
