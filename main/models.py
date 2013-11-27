46c09e66972383d596e28cac1005bde51323e236
Andrey Bulagin <abulagin@gmail.com>
2013-11-27 17:10:41 +0400

    thumbnails for uploaded background images

--- main/models.py
+++ main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+from PIL import Image
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.CharField(max_length=255)
@@ -33,10 +36,34 @@ class Category(models.Model):
     def has_subcategories(self):
         return len(self.subcategories) >= 2
 
-
     def facet_icon(self):
         return 'img/facet-' + self.slug + '-big-icon.png'
 
+    def save(self, background_size=(284, 300)):
+        super(Category, self).save()
 
+        # prepare thumbnail for uploaded background image
+        filename = self.background.path
+        image = Image.open(filename)
+    
+        src_width, src_height = image.size
+        src_ratio = float(src_width) / float(src_height)
+        dst_width, dst_height = background_size
+        dst_ratio = float(dst_width) / float(dst_height)
+        
+        if dst_ratio < src_ratio:
+            crop_height = src_height
+            crop_width = crop_height * dst_ratio
+            x_offset = int(float(src_width - crop_width) / 2)
+            y_offset = 0
+        else:
+            crop_width = src_width
+            crop_height = crop_width / dst_ratio
+            x_offset = 0
+            y_offset = int(float(src_height - crop_height) / 3)
+        image = image.crop((x_offset, y_offset, x_offset+int(crop_width), y_offset+int(crop_height)))
+        image = image.resize(background_size, Image.ANTIALIAS)
+        image.save(filename)
+    
     class Meta:
         verbose_name_plural = "categories"
