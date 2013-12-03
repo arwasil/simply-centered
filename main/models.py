a9bef57d57b433cfd369e0b709d493365b1ec975
Andrey Bulagin <abulagin@gmail.com>
2013-12-03 14:58:29 +0400

    fix work with missed images

--- main/models.py
+++ main/models.py
@@ -51,28 +51,32 @@ class Category(models.Model):
 
         if self.background:
             # prepare thumbnail for uploaded background image
-            image = Image.open(self.background)
+            try:
+                image = Image.open(self.background)
+            except:
+                image = None
         
-            src_width, src_height = image.size
-            src_ratio = float(src_width) / float(src_height)
-            dst_width, dst_height = background_size
-            dst_ratio = float(dst_width) / float(dst_height)
-            
-            if dst_ratio < src_ratio:
-                crop_height = src_height
-                crop_width = crop_height * dst_ratio
-                x_offset = int(float(src_width - crop_width) / 2)
-                y_offset = 0
-            else:
-                crop_width = src_width
-                crop_height = crop_width / dst_ratio
-                x_offset = 0
-                y_offset = int(float(src_height - crop_height) / 3)
-            image = image.crop((x_offset, y_offset, x_offset+int(crop_width), y_offset+int(crop_height)))
-            image = image.resize(background_size, Image.ANTIALIAS)
-            fh = storage.open(self.background.name, "w")
-            image.save(fh)
-            fh.close()
+            if image:
+                src_width, src_height = image.size
+                src_ratio = float(src_width) / float(src_height)
+                dst_width, dst_height = background_size
+                dst_ratio = float(dst_width) / float(dst_height)
+                
+                if dst_ratio < src_ratio:
+                    crop_height = src_height
+                    crop_width = crop_height * dst_ratio
+                    x_offset = int(float(src_width - crop_width) / 2)
+                    y_offset = 0
+                else:
+                    crop_width = src_width
+                    crop_height = crop_width / dst_ratio
+                    x_offset = 0
+                    y_offset = int(float(src_height - crop_height) / 3)
+                image = image.crop((x_offset, y_offset, x_offset+int(crop_width), y_offset+int(crop_height)))
+                image = image.resize(background_size, Image.ANTIALIAS)
+                fh = storage.open(self.background.name, "w")
+                image.save(fh)
+                fh.close()
     
     class Meta:
         verbose_name_plural = "categories"
