c3efacf0a1c363415cd9b0635049f1ca0aee2d7a
Andrey Bulagin <abulagin@gmail.com>
2013-12-19 13:35:37 +0400

    Search

--- main/api/spling.py
+++ main/api/spling.py
@@ -1,5 +1,26 @@
 import requests, simplejson as json
 
+def get_spling_data(params):
+    url = 'http://7gportal.spling.com/api/2/recommendation/'
+    response = requests.get(url, params=params)
+
+    try:
+        spling_data = response.json()['items']
+    except:
+        spling_data = []
+    
+    for data in spling_data:
+        data['type'] = data['MediaType']['name']
+
+        # show lightbox for videos and pictures
+        data['lightbox'] = data['type'] in ['Video', 'Picture']
+
+        # fix link for youtube items
+        if data['type'] == 'Video':
+            data['Link'] = data['Link'].replace('watch?v=', 'embed/').replace('&feature=', '?feature=')
+
+    return spling_data
+
 def recommendations(category, area, spec_limit=None):
     limit = 8
     limit = limit - min(category.sub_categories(area).count(), 4)
@@ -10,8 +31,6 @@ def recommendations(category, area, spec_limit=None):
     if spec_limit:
         limit = spec_limit
 
-    spling_data = []
-
     if limit > 0:
         params = {
             'limit': limit,
@@ -19,22 +38,19 @@ def recommendations(category, area, spec_limit=None):
             'tags': [category.name],
             'thumbnails[]': ['300x300', '140x300', '140x620']
         }
-
-        url = 'http://7gportal.spling.com/api/2/recommendation/'
-        response = requests.get(url, params=params)
-        try:
-            spling_data = response.json()['items']
-        except:
-            pass
-
         
-        for data in spling_data:
-            # fix link for youtube items
-            if data['EmbedlyType'] in [0, 1, 2]:
-                data['Link'] = data['Link'].replace('watch?v=', 'embed/').replace('&feature=', '?feature=')
-            
-            data['type'] = data['MediaType']['name']
-            data['lightbox'] = data['type'] in ['Video', 'Picture']
+        return get_spling_data(params)
+    else:
+        return []
 
+def search(query):
+    tags = query.split(' ')
+    print query
+    print tags
+    params = {
+        'format': 'json',
+        'tags': tags
+    }
+    
+    return get_spling_data(params)
 
-    return spling_data
